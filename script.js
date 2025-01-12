48ee2528d145a37f521058d9247d6c497fb74786
tawhidul7824262 <nvmilied1999@gmail.com>
2025-01-12 11:10:59 +0600

    todays task

---
+++ script.js
@@ -0,0 +1,33 @@
+var x=10;
+console.log(x);
+
+console.log(typeof(x));
+var y=20;
+var a="john";
+console.log(typeof(a));
+var z=x+y;
+console.log(z);
+
+function myfunction(){
+    let i=0;
+    return i;
+}
+console.log(myfunction());
+for(i =0;i>10;i++){
+    console.log(i);
+}
+// "John" integer(int)/float/double true/false(1/0) [12,12,13] {fname:,lanme:} 
+time=12;
+if(time==10){
+    console.log("it's time 10");
+}else{
+    console.log("it's not 10");
+}
+for(i=0;i<10;i++){
+    console.log(i);
+}
+var num=10;
+while(num<20){
+    console.log(num);
+    num++;
+}
